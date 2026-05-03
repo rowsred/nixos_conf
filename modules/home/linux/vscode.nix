d22ac7800f1280eb9cfd0b61b622fc3330d6cdb0
rowsred <fadlidev99@gmail.com>
2026-05-03 10:36:13 +0000

    vscode update config

--- modules/home/linux/vscode.nix
+++ modules/home/linux/vscode.nix
@@ -10,7 +10,7 @@
       programs.vscode = {
         enable = true;
         package = (
-          pkgs.vscodium.override {
+          pkgs.vscode.override {
             commandLineArgs = [
               "--ozone-platform-hint=auto"
               "--ozone-platform=wayland"
@@ -18,80 +18,115 @@
             ];
           }
         );
-        extensions = with pkgs.vscode-extensions; [
-          vscodevim.vim
-        ];
+        profiles = {
+          default = {
+            keybindings = [
+              {
+                key = "ctrl+j";
+                command = "workbench.action.terminal.toggleTerminal";
+                when = "terminal.active"; # Opsional: agar hanya aktif saat terminal relevan
+              }
+              {
+                key = "ctrl+j";
+                command = "workbench.action.terminal.toggleTerminal";
+                when = "!terminal.active";
+              }
+            ];
 
-        userSettings = {
-          "editor.minimap.enabled" = false;
-          "window.titleBarStyle" = "native";
-          "window.menuBarVisibility" = "toggle";
-          "window.customTitleBarVisibility" = "never";
+            extensions = with pkgs.vscode-extensions; [
+              sumneko.lua
+              vscodevim.vim
+              jnoortheen.nix-ide
+              pkief.material-icon-theme
+              usernamehw.errorlens
+            ];
 
-          "vim.leader" = "<space>";
-          "vim.insertModeKeyBindings" = [
-            {
-              "before" = [
-                "j"
-                "k"
-              ];
-              "after" = [ "<Esc>" ];
-            }
-          ];
-          "vim.normalModeKeyBindingsNonRecursive" = [
+            userSettings = {
+              "editor.scrollbar.vertical" = "hidden";
+              "editor.scrollbar.horizontal" = "hidden";
+              "editor.scrollbar.verticalScrollbarSize" = 0;
+              "editor.scrollbar.horizontalScrollbarSize" = 0;
+              "editor.folding" = false;
+              "workbench.notification.collapse" = true;
 
-            {
-              "before" = [
-                "leader"
-                "c"
-              ];
-              "commands" = [ "workbench.action.closeActiveEditor" ];
-            }
+              #this for remove topbar
+              "workbench.editor.editorActionsLocation" = "hidden";
+              "workbench.iconTheme" = "material-icon-theme";
+              "breadcrumbs.enabled" = false;
+              "editor.minimap.enabled" = false;
+              "window.titleBarStyle" = "native";
+              "window.menuBarVisibility" = "toggle";
+              "window.customTitleBarVisibility" = "never";
+              #for nix config
+              "nix.enableLanguageServer" = true;
 
-            {
-              "before" = [
-                "leader"
-                "x"
+              "vim.leader" = "<space>";
+              "vim.insertModeKeyBindings" = [
+                {
+                  "before" = [
+                    "j"
+                    "k"
+                  ];
+                  "after" = [ "<Esc>" ];
+                }
               ];
-              "commands" = [ "workbench.action.previousEditor" ];
-            }
+              "vim.normalModeKeyBindingsNonRecursive" = [
 
-            {
-              "before" = [
-                "leader"
-                "l"
+                {
+                  "before" = [
+                    "leader"
+                    "c"
+                  ];
+                  "commands" = [ "workbench.action.closeActiveEditor" ];
+                }
+
+                {
+                  "before" = [
+                    "leader"
+                    "x"
+                  ];
+                  "commands" = [ "workbench.action.previousEditor" ];
+                }
+
+                {
+                  "before" = [
+                    "leader"
+                    "l"
+                  ];
+                  "commands" = [ "workbench.action.nextEditor" ];
+                }
+                {
+                  "before" = [
+                    "leader"
+                    "e"
+                  ];
+                  "commands" = [ "workbench.view.explorer" ];
+                }
+                {
+                  "before" = [
+                    "leader"
+                    "w"
+                  ];
+                  "commands" = [ "workbench.action.files.save" ];
+                }
+                {
+                  "before" = [
+                    "leader"
+                    "f"
+                  ];
+                  "commands" = [ "workbench.action.quickOpen" ];
+                }
               ];
-              "commands" = [ "workbench.action.nextEditor" ];
-            }
-            {
-              "before" = [
-                "leader"
-                "e"
-              ];
-              "commands" = [ "workbench.view.explorer" ];
-            }
-            {
-              "before" = [
-                "leader"
-                "w"
-              ];
-              "commands" = [ "workbench.action.files.save" ];
-            }
-            {
-              "before" = [
-                "leader"
-                "f"
-              ];
-              "commands" = [ "workbench.action.quickOpen" ];
-            }
-          ];
-          "editor.fontFamily" = "'SFMono Nerd Font', 'monospace'";
-          "editor.fontSize" = 12;
-          "vim.useSystemClipboard" = true;
-          "vim.hlsearch" = true;
-          "editor.formatOnSave" = true;
+
+              "editor.fontFamily" = "'SFMono Nerd Font', 'monospace'";
+              "editor.fontSize" = 12;
+              "vim.useSystemClipboard" = true;
+              "vim.hlsearch" = true;
+              "editor.formatOnSave" = true;
+            };
+          };
+
         };
       };
-
     };
 }
